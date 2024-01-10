fe256329c8d989cddae07ac225c350333855f116
Marcel Singer <marcel.singer@lise.de>
2024-01-10 18:30:44 +0100

    Make the :stereotyped package part of :springboot's api interface

--- library/springboot/build.gradle.kts
+++ library/springboot/build.gradle.kts
@@ -1,18 +1,22 @@
 dependencies {
-    implementation("org.springframework:spring-context:6.0.13")
-    implementation("org.springframework.boot:spring-boot-autoconfigure:3.1.5")
-    
-    implementation(kotlin("reflect"))
-    
     api(project(":api"))
     api(project(":engine"))
-    runtimeOnly(project(":springboot-test-scheduling"))
+    api(project(":stereotyped"))
 
-    implementation(project(":stereotyped"))
+
+    implementation(kotlin("reflect"))
+
+    implementation("org.springframework:spring-context:6.0.13")
+    implementation("org.springframework.boot:spring-boot-autoconfigure:3.1.5")
+
     implementation(project(":reflection"))
     implementation(project(":persistence"))
     implementation(project(":scheduling"))
     implementation(project(":validation"))
+
+
+    runtimeOnly(project(":springboot-test-scheduling"))
+
 
     testImplementation(project(":testing"))
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.0")
