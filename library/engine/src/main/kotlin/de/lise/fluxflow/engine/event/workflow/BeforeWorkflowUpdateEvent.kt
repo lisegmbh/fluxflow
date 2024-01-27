141d5e27f86893478bcf0ca356e6c3734a64d95f
Marcel Singer <marcel.singer@lise.de>
2024-01-27 17:27:56 +0100

    Check if workflow data actually changed before persisting them

--- library/engine/src/main/kotlin/de/lise/fluxflow/engine/event/workflow/BeforeWorkflowUpdateEvent.kt
+++ library/engine/src/main/kotlin/de/lise/fluxflow/engine/event/workflow/BeforeWorkflowUpdateEvent.kt
@@ -2,4 +2,14 @@ package de.lise.fluxflow.engine.event.workflow
 
 import de.lise.fluxflow.api.workflow.Workflow
 
+/**
+ * This event is raised immediately whenever FluxFlow is requested to persist changes to a workflow.
+ * 
+ * **Important**:
+ * The event will be raised unconditionally every time a persist operation is **requested**.
+ * This holds true,
+ * even if FluxFlow later determines
+ * that the persist operation can be skipped because the workflow didn't really change.
+ */
+@Deprecated("This event will not be raised in future versions of FluxFlow and is going to be removed.")
 class BeforeWorkflowUpdateEvent(workflow: Workflow<*>): WorkflowEvent(workflow)
--- library/engine/src/main/kotlin/de/lise/fluxflow/engine/workflow/WorkflowUpdateServiceImpl.kt
+++ library/engine/src/main/kotlin/de/lise/fluxflow/engine/workflow/WorkflowUpdateServiceImpl.kt
@@ -1,24 +1,38 @@
 package de.lise.fluxflow.engine.workflow
 
 import de.lise.fluxflow.api.event.EventService
+import de.lise.fluxflow.api.state.ChangeDetector
 import de.lise.fluxflow.api.workflow.Workflow
 import de.lise.fluxflow.api.workflow.WorkflowUpdateService
 import de.lise.fluxflow.engine.event.workflow.BeforeWorkflowUpdateEvent
 import de.lise.fluxflow.engine.event.workflow.WorkflowUpdatedEvent
+import de.lise.fluxflow.persistence.workflow.WorkflowData
 import de.lise.fluxflow.persistence.workflow.WorkflowPersistence
+import org.slf4j.LoggerFactory
 
 class WorkflowUpdateServiceImpl(
     private val persistence: WorkflowPersistence,
+    private val changeDetector: ChangeDetector<WorkflowData>,
     private val eventService: EventService,
 ) : WorkflowUpdateService {
     override fun <TWorkflowModel> saveChanges(workflow: Workflow<TWorkflowModel>): Workflow<TWorkflowModel> {
         eventService.publish(BeforeWorkflowUpdateEvent(workflow))
 
-        val data = persistence.find(workflow.id)!!
-        val updatedWorkflow = persistence.save(data.withModel(workflow.model))
+        val oldWorkflowData = persistence.find(workflow.id)!!
+        val updatedWorkflowData = oldWorkflowData.withModel(workflow.model)
+        if(!changeDetector.hasChanged(oldWorkflowData, updatedWorkflowData)) {
+            Logger.debug("Skip persisting/updating workflow '{}' as it hasn't changed.", workflow.id.value)
+            return workflow
+        }
+        
+        val updatedWorkflow = persistence.save(updatedWorkflowData)
 
         eventService.publish(WorkflowUpdatedEvent(workflow))
 
         return WorkflowImpl(updatedWorkflow)
+    }
+    
+    private companion object {
+        private val Logger = LoggerFactory.getLogger(WorkflowStarterServiceImpl::class.java)!! 
     }
 }
