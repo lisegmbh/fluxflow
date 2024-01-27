145007c8dd53ec2efca3334a45e0b63a75e855cb
Marcel Singer <marcel.singer@lise.de>
2024-01-27 21:30:49 +0100

    Add KDoc for Step and StatefulStep

--- library/api/src/main/kotlin/de/lise/fluxflow/api/step/Step.kt
+++ library/api/src/main/kotlin/de/lise/fluxflow/api/step/Step.kt
@@ -1,12 +1,41 @@
 package de.lise.fluxflow.api.step
 
+import de.lise.fluxflow.api.step.stateful.StatefulStep
 import de.lise.fluxflow.api.workflow.Workflow
 
+/**
+ * The [Step] API element represents a step within a workflow.
+ *
+ * Known subtypes are:
+ * - [StatefulStep]: Used for steps that are stateful (e.g., having step data, exposing actions).
+ */
 interface Step {
+    /**
+     * The unique identifier that can be used to identify this step.
+     *
+     * **Note that** the identifier is only guaranteed to be unique within the executing workflow.
+     * It is possible that different workflows are having steps with the same identifier.
+     */
     val identifier: StepIdentifier
+
+    /**
+     * The [StepDefinition] describing this step.
+     */
     val definition: StepDefinition
+
+    /**
+     * The workflow this step belongs to.
+     */
     val workflow: Workflow<*>
+
+    /**
+     * This step's current status.
+     */
     val status: Status
+
+    /**
+     * Arbitrary metainformation associated with this step.
+     */
     val metadata: Map<String, Any>
 }
 
--- library/api/src/main/kotlin/de/lise/fluxflow/api/step/stateful/StatefulStep.kt
+++ library/api/src/main/kotlin/de/lise/fluxflow/api/step/stateful/StatefulStep.kt
@@ -4,7 +4,18 @@ import de.lise.fluxflow.api.step.Step
 import de.lise.fluxflow.api.step.stateful.action.Action
 import de.lise.fluxflow.api.step.stateful.data.Data
 
+/**
+ * A [StatefulStep] is a step with associated state information.
+ * Such a step can define and hold data and provide executable actions.
+ */
 interface StatefulStep : Step {
+    /**
+     * The data that is stored for this step.
+     */
     val data: List<Data<*>>
+
+    /**
+     * A list of actions that are executable for this step.
+     */
     val actions: List<Action>
 }
