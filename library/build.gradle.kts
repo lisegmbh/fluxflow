1314ecc148def6c529052228fa4090ad1b1a053c
Marcel Singer <marcel.singer@lise.de>
2024-01-28 19:15:54 +0100

    Upgrade junit API and engine

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -57,9 +57,9 @@ subprojects {
         // Test
         testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
         testImplementation("org.assertj:assertj-core:3.24.2")
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
         testImplementation("org.mockito:mockito-inline:5.2.0")
 
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
     }
 
