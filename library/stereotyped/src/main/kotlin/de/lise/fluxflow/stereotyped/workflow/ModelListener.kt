044c802cc2168a2a925a604a4ad5aa112dc78647
Marcel Singer <marcel.singer@lise.de>
2024-02-17 18:24:09 +0100

    Add optional selector to ModelListener

--- library/stereotyped/src/main/kotlin/de/lise/fluxflow/stereotyped/workflow/ModelListener.kt
+++ library/stereotyped/src/main/kotlin/de/lise/fluxflow/stereotyped/workflow/ModelListener.kt
@@ -3,8 +3,17 @@ package de.lise.fluxflow.stereotyped.workflow
 /**
  * The `@ModelListener` is used to register a workflow model's function as a model listener.
  * Functions annotated with this annotation are invoked whenever the workflow's model changes.
+ * @param value A selector that selects the relevant part of the workflow model,
+ * that must change in order for this listener to be invoked.
+ *
+ * If the value is an empty string, any model change will trigger this listener.
+ * The expression syntax depends on the current runtime.
+ *
+ * In Spring environments, this will usually be the Spring Expression Language.
  */
 @Repeatable
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ModelListener
+annotation class ModelListener(
+    val value: String = ""
+)
