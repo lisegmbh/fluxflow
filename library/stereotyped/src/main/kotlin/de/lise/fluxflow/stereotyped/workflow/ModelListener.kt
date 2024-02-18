c814e78ec4e847b0abde6b4f4548d36f2ac327bd
Marcel Singer <marcel.singer@lise.de>
2024-02-18 16:34:13 +0100

    Allow ModelListener selectors to directly decide whenever a model has relevant changes

--- library/stereotyped/src/main/kotlin/de/lise/fluxflow/stereotyped/workflow/ModelListener.kt
+++ library/stereotyped/src/main/kotlin/de/lise/fluxflow/stereotyped/workflow/ModelListener.kt
@@ -3,17 +3,26 @@ package de.lise.fluxflow.stereotyped.workflow
 /**
  * The `@ModelListener` is used to register a workflow model's function as a model listener.
  * Functions annotated with this annotation are invoked whenever the workflow's model changes.
- * @param value A selector that selects the relevant part of the workflow model,
- * that must change in order for this listener to be invoked.
+ * @param selector A selector expression that selects the relevant aspect of a workflow's model,
+ * which must change in order for this listener to be invoked.
  *
  * If the value is an empty string, any model change will trigger this listener.
  * The expression syntax depends on the current runtime.
  *
- * In Spring environments, this will usually be the Spring Expression Language.
+ * In Spring environments, the expression syntax will usually be the Spring Expression Language.
+ * @param selectorReturnsDecision If set to `true`, the selector should return a boolean value.
+ *
+ * The result will then directly indicate if there are relevant changes to the model.
+ *
+ * If the parameter is set to false (which is the default),
+ * the expression is expected to return any part of the model and will be invoked twice.
+ * Once for the "old" model and a second time for the updated model.
+ * Only if the returned values differ, the listener will be invoked.
  */
 @Repeatable
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class ModelListener(
-    val value: String = ""
+    val selector: String = "",
+    val selectorReturnsDecision: Boolean = false,
 )
