c380ddf55c0990fb29720797df9f8a828719877c
Marcel Singer <marcel.singer@lise.de>
2024-02-18 17:02:40 +0100

    Use selector to build change conditions

--- library/stereotyped/src/test/kotlin/de/lise/fluxflow/stereotyped/workflow/ModelListenerDefinitionBuilderIT.kt
+++ library/stereotyped/src/test/kotlin/de/lise/fluxflow/stereotyped/workflow/ModelListenerDefinitionBuilderIT.kt
@@ -4,13 +4,22 @@ import de.lise.fluxflow.api.workflow.Workflow
 import de.lise.fluxflow.reflection.activation.parameter.ParameterResolver
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 
 class ModelListenerDefinitionBuilderIT {
     @Test
     fun `build should return a definition if there is one`() {
         val parameterResolver = mock<ParameterResolver> {}
-        val builder = ModelListenerDefinitionBuilder(parameterResolver)
+        val expressionParser = mock<SelectorExpressionParser> {
+            on { parse(any()) } doReturn SelectorExpression{ _, _, _ -> true }
+        }
+        val builder = ModelListenerDefinitionBuilder(
+            parameterResolver,
+            { old, new -> old != new },
+            expressionParser
+        )
         val model = TestModelWithListener()
 
         // Act
@@ -23,7 +32,14 @@ class ModelListenerDefinitionBuilderIT {
     @Test
     fun `build should return a definition that's listener receives all provided values`() {
         val parameterResolver = mock<ParameterResolver> {}
-        val builder = ModelListenerDefinitionBuilder(parameterResolver)
+        val expressionParser = mock<SelectorExpressionParser> {
+            on { parse(any()) } doReturn SelectorExpression{ _, _, _ -> true }
+        }
+        val builder = ModelListenerDefinitionBuilder(
+            parameterResolver,
+            { old, new -> old != new },
+            expressionParser
+        )
         val oldModel = TestModelWithListener()
         val currentModel = TestModelWithListener()
         val workflow = mock<Workflow<TestModelWithListener>> {}
@@ -42,7 +58,14 @@ class ModelListenerDefinitionBuilderIT {
     @Test
     fun `build should not return a definition if there is none`() {
         val parameterResolver = mock<ParameterResolver> {}
-        val builder = ModelListenerDefinitionBuilder(parameterResolver)
+        val expressionParser = mock<SelectorExpressionParser> {
+            on { parse(any()) } doReturn SelectorExpression { _, _, _ -> true }
+        }
+        val builder = ModelListenerDefinitionBuilder(
+            parameterResolver,
+            { old, new -> old != new },
+            expressionParser
+        )
         val model = TestModelWithoutListener()
 
         // Act
@@ -62,7 +85,8 @@ class ModelListenerDefinitionBuilderIT {
         fun hasChanged(
             workflow: Workflow<TestModelWithListener>,
             oldValue: TestModelWithListener,
-            newValue: TestModelWithListener) {
+            newValue: TestModelWithListener
+        ) {
             this.workflow = workflow
             this.oldValue = oldValue
             this.newValue = newValue
