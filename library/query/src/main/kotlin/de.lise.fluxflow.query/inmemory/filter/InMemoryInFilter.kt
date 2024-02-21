44cbfe4254edd6c84b52d39d72d21be3f87d1932
Marcel Singer <marcel.singer@lise.de>
2024-02-21 11:04:23 +0100

    Rename InMemoryAnyOfFilter to InMemoryInFilter

---
+++ library/query/src/main/kotlin/de.lise.fluxflow.query/inmemory/filter/InMemoryInFilter.kt
@@ -0,0 +1,23 @@
+package de.lise.fluxflow.query.inmemory.filter
+
+import de.lise.fluxflow.query.filter.InFilter
+
+@Deprecated(
+    "This alias will be removed in an upcoming version of FluxFlow. Use InMemoryInFilter<TModel> instead.",
+    ReplaceWith("InMemoryInFilter<TModel>")
+)
+typealias InMemoryAnyOfFilter<TModel> = InMemoryInFilter<TModel>
+
+class InMemoryInFilter<TModel>(
+    private val values: List<TModel>,
+) : InMemoryFilter<TModel> {
+    constructor(inFilter: InFilter<TModel>) : this(
+        inFilter.anyOfValues
+    )
+
+    override fun toPredicate(): InMemoryPredicate<TModel> {
+        return InMemoryPredicate { value ->
+            values.contains(value)
+        }
+    }
+}
