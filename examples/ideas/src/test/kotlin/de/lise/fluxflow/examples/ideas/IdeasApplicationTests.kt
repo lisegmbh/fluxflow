d4df23caaaeaea05ac869f85a2160137f73ad839
Marcel Singer <marcel.singer@lise.de>
2024-03-03 22:10:06 +0100

    Add getting started section including a project setup tutorial

---
+++ examples/ideas/src/test/kotlin/de/lise/fluxflow/examples/ideas/IdeasApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.lise.fluxflow.examples.ideas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdeasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
