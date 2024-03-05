6adc63fe6faafbcdf0ac00ff53b4ccfffb4ea78d
Marcel Singer <marcel.singer@lise.de>
2024-03-05 07:09:20 +0100

    Throw exception if dependency resolution fails for other reasons than a non-existing bean

--- library/springboot/springboot/src/main/kotlin/de/lise/fluxflow/springboot/ioc/SpringIocProvider.kt
+++ library/springboot/springboot/src/main/kotlin/de/lise/fluxflow/springboot/ioc/SpringIocProvider.kt
@@ -1,7 +1,9 @@
 package de.lise.fluxflow.springboot.ioc
 
 import de.lise.fluxflow.api.ioc.IocProvider
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.NoSuchBeanDefinitionException
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException
 import org.springframework.context.ApplicationContext
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
@@ -10,19 +12,30 @@ class SpringIocProvider(
     private val applicationContext: ApplicationContext
 ) : IocProvider {
     override fun <TDependency : Any> provide(type: KClass<out TDependency>): TDependency? {
-        return try {
-            applicationContext.getBean(type.java)
-        } catch (e: NoSuchBeanDefinitionException) {
-            null
-        }
+        return provide(type.java)
     }
 
     override fun provide(type: KType): Any? {
         val javaClass = (type.classifier as? KClass<*>)?.java
+        return provide(javaClass as Class<*>)
+    }
+
+    private fun <TDependency : Any> provide(clazz: Class<TDependency>): TDependency? {
         return try {
-            applicationContext.getBean(javaClass)
+            applicationContext.getBean(clazz)
+        } catch (e: NoUniqueBeanDefinitionException) {
+            throw e
         } catch (e: NoSuchBeanDefinitionException) {
+            Logger.debug(
+                "Could not find a bean of type '{}' in the application context.",
+                clazz.simpleName,
+                e
+            )
             null
         }
+    }
+
+    private companion object {
+        val Logger = LoggerFactory.getLogger(SpringIocProvider::class.java)!!
     }
 }
