ae32a3254d3d1544b93751e188dcea803241234e
Marcel Singer <marcel.singer@lise.de>
2024-03-17 16:46:36 +0100

    Implement MigrationService and extend MigrationPersistence

--- library/core/migration/src/main/kotlin/de/lise/fluxflow/migration/MigrationService.kt
+++ library/core/migration/src/main/kotlin/de/lise/fluxflow/migration/MigrationService.kt
@@ -8,6 +8,7 @@ interface MigrationService {
      * Applies the given migration.
      * If the migration has already been applied (evaluated using the [Migration.key]), this method does nothing.
      * @param migration the migration to be applied.
+     * @throws UnsupportedMigrationException if there is no provider that supports the given migration.
      */
     fun apply(migration: Migration)
 }
--- library/core/persistence/src/main/kotlin/de/lise/fluxflow/persistence/migration/MigrationPersistence.kt
+++ library/core/persistence/src/main/kotlin/de/lise/fluxflow/persistence/migration/MigrationPersistence.kt
@@ -11,4 +11,11 @@ interface MigrationPersistence {
      * @throws DuplicateMigrationException if a different migration with the same key has already been persisted.
      */
     fun persist(migration: MigrationData): MigrationData
+
+    /**
+     * Finds a migration by its key.
+     * @param key the key of the migration to be found.
+     * @return the migration with the given key or `null` if no such migration exists.
+     */
+    fun find(key: String): MigrationData?
 }
