807c4934f467509a20b3579d8d50c4adfb8eeb42
Marcel Singer <marcel.singer@lise.de>
2024-03-17 19:48:21 +0100

    Add TypeRenameMigration and implementation for in-memory persistence

---
+++ library/core/migration/src/main/kotlin/de/lise/fluxflow/migration/common/TypeRenameMigration.kt
@@ -0,0 +1,31 @@
+package de.lise.fluxflow.migration.common
+
+import de.lise.fluxflow.migration.Migration
+import de.lise.fluxflow.migration.MigrationKey
+import kotlin.reflect.KClass
+
+/**
+ * A [TypeRenameMigration] can be used to migrate/rename an old type or kind.
+ * @param originalName the original name as used before applying the migration.
+ * @param newName the new name the original should be renamed to.
+ */
+open class TypeRenameMigration(
+    val originalName: String,
+    val newName: String,
+) : Migration {
+    override val key: MigrationKey
+        get() = MigrationKey("rename-${originalName}-to-${newName}")
+
+    /**
+     * Creates a new [TypeRenameMigration].
+     * @param originalName the original name as used before applying the migration.
+     * @param newType the new type the original should be renamed to.
+     */
+    constructor(
+        originalName: String,
+        newType: KClass<*>
+    ) : this(
+        originalName,
+        newType.qualifiedName ?: newType.java.canonicalName
+    )
+}
