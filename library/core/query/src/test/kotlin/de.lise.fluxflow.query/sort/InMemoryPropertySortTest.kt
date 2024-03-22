e5192c5ce1e918e4c13cac1beaf784784a0c2f4e
Christian Scholz <christian.scholz@lise.de>
2024-03-22 13:36:42 +0100

    Add tests for in memory nullable property sort

--- library/core/query/src/test/kotlin/de.lise.fluxflow.query/sort/InMemoryPropertySortTest.kt
+++ library/core/query/src/test/kotlin/de.lise.fluxflow.query/sort/InMemoryPropertySortTest.kt
@@ -28,6 +28,55 @@ class InMemoryPropertySortTest {
             assertThat(result).containsExactly(*sortedList.toTypedArray())
         }
     }
+
+    @Test
+    fun `property sort should correctly order objects ascending by nullable property`() {
+        // Arrange
+        val sort = InMemoryPropertySort(
+            TestObjectWithNullableValue::value,
+            InMemoryDefaultSort(Direction.Ascending)
+        )
+
+        val sortedList = listOf(
+            TestObjectWithNullableValue(null),
+            TestObjectWithNullableValue(1),
+            TestObjectWithNullableValue(2),
+            TestObjectWithNullableValue(3),
+        )
+
+        // Act & Assert
+        repeat(10) {
+            val shuffled = sortedList.shuffled()
+            val result = shuffled.sortedWith(sort.toComparator())
+
+            assertThat(result).containsExactly(*sortedList.toTypedArray())
+        }
+    }
+
+    @Test
+    fun `property sort should correctly order objects descending by nullable property`() {
+        // Arrange
+        val sort = InMemoryPropertySort(
+            TestObjectWithNullableValue::value,
+            InMemoryDefaultSort(Direction.Descending)
+        )
+
+        val sortedList = listOf(
+            TestObjectWithNullableValue(null),
+            TestObjectWithNullableValue(3),
+            TestObjectWithNullableValue(2),
+            TestObjectWithNullableValue(1),
+        )
+
+        // Act & Assert
+        repeat(10) {
+            val shuffled = sortedList.shuffled()
+            val result = shuffled.sortedWith(sort.toComparator())
+
+            assertThat(result).containsExactly(*sortedList.toTypedArray())
+        }
+    }
 }
 
 private data class TestObject(val value: Number)
+private data class TestObjectWithNullableValue(val value: Number?)
