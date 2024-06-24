5cfe43caaa1f4d8cca48d3a57ec15bd5d94788d0
Marcel Singer <marcel.singer@lise.de>
2024-06-24 10:23:07 +0200

    Add step definition persistence types and API interfaces

---
+++ library/core/persistence/src/main/kotlin/de/lise/fluxflow/persistence/step/definition/DataDefinitionData.kt
@@ -0,0 +1,8 @@
+package de.lise.fluxflow.persistence.step.definition
+
+data class DataDefinitionData(
+    val kind: String,
+    val type: String,
+    val metadata: Map<String, Any>,
+    val isCalculatedValue: Boolean,
+)
