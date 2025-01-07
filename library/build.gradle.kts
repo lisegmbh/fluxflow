6704ce64db2b47a1f2e3f9bf81fb0108834c5b24
Christian Scholz <christian.scholz@lise.de>
2025-01-07 13:04:20 +0100

    Increase snapshot version

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -31,7 +31,7 @@ subprojects {
     apply(plugin = "io.spring.dependency-management")
     
     group = "de.lise.fluxflow"
-    version = projVersion ?: "0.2.0-SNAPSHOT-9"
+    version = projVersion ?: "0.2.0-SNAPSHOT-10"
 
     repositories {
         mavenCentral()
