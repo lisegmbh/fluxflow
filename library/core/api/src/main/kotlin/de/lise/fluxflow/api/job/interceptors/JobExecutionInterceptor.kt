10200cd629b21aeac3b6fecf6f06f69e41ded027
Marcel Singer <marcel.singer@lise.de>
2025-09-02 13:00:33 +0200

    Add KDoc for the JobExecutionInterceptor that highlights the implications of cancelling a job execution

--- library/core/api/src/main/kotlin/de/lise/fluxflow/api/job/interceptors/JobExecutionInterceptor.kt
+++ library/core/api/src/main/kotlin/de/lise/fluxflow/api/job/interceptors/JobExecutionInterceptor.kt
@@ -2,4 +2,15 @@ package de.lise.fluxflow.api.job.interceptors
 
 import de.lise.fluxflow.api.interceptors.FlowInterceptor
 
+/**
+ * A [JobExecutionInterceptor] can be used to intercept immediate job executions.
+ *
+ * **Note:** Cancelling an execution prevents FluxFlow from updating any information
+ * related to the affected job, including (but not limited to) its status.
+ *
+ * Interceptors that cancel a job execution are responsible for performing the necessary actions, such as:
+ * - updating the job status (according to the cancellation reason)
+ * - emitting required events
+ * - scheduling required continuations
+ */
 interface JobExecutionInterceptor : FlowInterceptor<JobExecutionContext>
